72d64f880e4866bab07d402a83d2658ddca1534f
kpagariya <38912672+kpagariya@users.noreply.github.com>
2024-08-04 11:52:12 +0530

    Update main.js

--- docker_node_app/main.js
+++ docker_node_app/main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.end("<H2>Hello Docker - Node App !! \n</H2>");
+  response.end("<H2>Hello Docker - Node App running in container!! \n</H2>");
 });
 
 // listen on localhost:8009
