965743e8b86e0eddae34ec3641e1fa593fc6fd8c
kpagariya <kunal.pagariya@gmail.com>
2024-09-21 18:26:43 +0530

    updated pom.xml

--- java-project/src/main/java/com/example/App.java
+++ java-project/src/main/java/com/example/App.java
@@ -3,7 +3,7 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-        port(8080); // Spark will run on port 8080 inside the container
+        port(8081); // Spark will run on port 8080 inside the container
         get("/", (req, res) -> "Hello, World!");
     }
 }
